38459dd6b89c2e3a51f41e063f54e343be2031e8
kdh6194 <kdh6194@naver.com>
2023-02-22 13:12:19 +0900

    시도,구군,읍면동 출력 - 시도 : 시군구

--- routes/zipcode.js
+++ routes/zipcode.js
@@ -3,18 +3,23 @@ const router = express.Router();
 const Zipcode = require('../models/Zipcode')
 
 router.get('/zipcode',async (req, res)=>{
-    let sidos = new Zipcode().getSido().then((result)=>result)
+    let sidos = new Zipcode().getSido().then((result)=>result);
     // console.log(await sidos)
 
-    let sido = '서울'
-    let guguns = new Zipcode().getGugun(sido).then((result)=>result)
-    // console.log(await guguns)
+    let sido = req.query.sido;
+    let gugun = req.query.gugun;
+    let dong = req.query.dong;
+    let [guguns, dongs, zips] = [null,null,null];
 
-    let gugun = '강남구'
-    let dongs = new Zipcode().getDong(sido,gugun).then((result)=>result)
+    if(sido !== undefined) {
+       guguns = new Zipcode().getGugun(sido).then((result)=>result)}
+    // console.log(await guguns)
+    if(sido !== undefined && gugun !== undefined) {
+        dongs = new Zipcode().getDong(sido,gugun).then((result)=>result)}
     // console.log(await dongs)
 
-    res.render('zipcode',{title:'시군구동 찾기',sidos:await sidos,guguns:await guguns,dongs:await dongs});
+    res.render('zipcode',{title:'시군구동 찾기',sidos:await sidos,guguns:await guguns,dongs:await dongs
+    , sido:sido, gugun:gugun, dong:dong});
 }); //app.js 라우터 경로에 zipcode를 설정을 안했기 때문에 '/'가 작동이 되지 않았음
     // 같은 경로의 router.get이 있으면 처음에 있는 명령만 동작 에러 발생은 나지 않음
     // 우편번호를 만들때는 한곳에 모아서 작성하자
